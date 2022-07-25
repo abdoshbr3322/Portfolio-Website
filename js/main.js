7f170de6d80dfe91ab9817342aaa23d4b6ab3e72
abdoshbr3322 <abdullahelshebrawy@gmail.com>
2022-07-25 08:55:37 +0200

    Finsihed Main Page And Header And Brands

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  // Open Mega Menu On Hover On Desktop
+  let menu = $("nav .nav-wrapper");
+  let overlay = $("nav .nav-overlay");
+
+  checkWindowSize();
+  $(window).resize(checkWindowSize);
+
+  function checkWindowSize() {
+    if (window.innerWidth >= 992) {
+      menu.removeClass("menu-active");
+      overlay.removeClass("menu-active");
+    }
+  }
+
+  // Open ,Close Menu And Nav On Click On Mobile Screen
+  let menuOpenerCloser = $(".menu-opener , .menu-closer");
+
+  menuOpenerCloser.click(function () {
+    let targetMenuSelector = $(this).attr("data-menu");
+    $(targetMenuSelector).toggleClass("menu-active");
+    overlay.toggleClass("menu-active");
+  });
+
+  // Close Menu On Blur
+
+  overlay.click(() => {
+    menu.removeClass("menu-active");
+    overlay.removeClass("menu-active");
+  });
+
+  // Show Header And Scroll Top Button On Scroll
+
+  let header = $("header");
+
+  changeOnScroll(header, "fixed", 335);
+
+  function changeOnScroll(el, className, scrollPosition) {
+    checkScrollPosition(el, className, scrollPosition);
+    $(window).on("scroll", () => {
+      checkScrollPosition(el, className, scrollPosition);
+    });
+  }
+
+  function checkScrollPosition(el, className, scrollPosition) {
+    if ($(window).scrollTop() >= scrollPosition) {
+      el.addClass(className);
+    } else {
+      el.removeClass(className);
+    }
+  }
+});
