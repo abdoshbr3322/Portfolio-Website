39cd0999d89216dac447e13727d89ee250e4f73c
abdoshbr3322 <abdullahelshebrawy@gmail.com>
2022-07-29 12:11:33 +0200

    finished testimonials and carousel

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  let carousel = $(".owl-carousel");
+
+  carousel.owlCarousel({
+    margin: 48,
+    stagePadding: 24,
+    loop: true,
+    dots: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    dotsClass: "bullets",
+    dotClass: "bullet",
+    onResize: resized,
+    onInitialize: resized,
+  });
+})
+
+function resized() {
+  this.options.items = (window.innerWidth <= 575 ? 1 : 2);
+}
